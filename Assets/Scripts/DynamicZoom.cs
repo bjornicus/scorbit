c01ece5689eced71251df6d27342701c1b454bbb
Bjorn Hansen <holomorph@gmail.com>
2016-12-16 22:22:38 -0800

    smooth step camera zooms

--- Assets/Scripts/DynamicZoom.cs
+++ Assets/Scripts/DynamicZoom.cs
@@ -5,16 +5,29 @@ public class DynamicZoom : MonoBehaviour {
 
 	public Transform keepInView;
 
+	private float previousCameraSize;
+	private float targetCameraSize;
+	private float startTime;
+	private float duration = 0.5f;
+
 	private Camera theCamera;
 	// Use this for initialization
 	void Start () {
 		theCamera = GetComponent<Camera> ();
+		previousCameraSize = targetCameraSize = theCamera.orthographicSize;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (keepInView != null) {
-			theCamera.orthographicSize = Mathf.Clamp (keepInView.position.magnitude + 2, 15, 100);
+			var desiredCameraSize = Mathf.Clamp(Mathf.NextPowerOfTwo(Mathf.RoundToInt(keepInView.position.magnitude)),16, 64);
+			if (desiredCameraSize != targetCameraSize) {
+				startTime = Time.time;
+				targetCameraSize = desiredCameraSize;
+				previousCameraSize = theCamera.orthographicSize;
+			}
+			float t = (Time.time - startTime) / duration;
+			theCamera.orthographicSize = Mathf.SmoothStep(previousCameraSize, targetCameraSize, t);	
 		}
 	}
 }
